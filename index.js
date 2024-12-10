5290f39dc4ac796c91cde46bfd190b9da2fe4450
Seth Martineau <martineausw@gmail.com>
2024-12-10 04:35:49 -0500

    add operators

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import { JSDOM } from 'jsdom';
 
 const jsdom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
-const document = jsdom.window.document;
+const document =  jsdom.window.document;
+const Node = jsdom.window.Node;
 const HTMLElement = jsdom.window.HTMLElement;
+const HTMLTemplateElement = jsdom.window.HTMLTemplateElement;
 
 function parseEmmetToObject(str) {
 
   const regex = new RegExp(
     `^(?<tag>[a-z]+)?` +                // tag
-    `(?<attributes>\\[[a-z-0-9=" ]+\\])?` + // attributes
+    `(?<attributes>\\[[a-z-0-9=" ]+?\\])?` + // attributes
     `(?<id>#[a-z-0-9]+)?` +             // id
     `(?<classes>(?:\\.[a-z-0-9]+)*)?` + // classes
-    `(?<textContent>{[\\s\\S]+})?`      // text content
+    `(?<textContent>{[\\s\\S]+?})?`      // text content
   );
 
   const groups = str.match(regex).groups;
@@ -20,9 +22,54 @@ function parseEmmetToObject(str) {
     tag: groups.tag ? groups.tag : 'div',
     attributes: attributeMapFromString(groups.attributes),
     id: groups.id ? groups.id.substring(1) : undefined,
-    classes: classListFromString(groups.classes), 
+    classes: classListFromString(groups.classes),
     textContent: groups.textContent ? groups.textContent.slice(1, -1) : undefined,
   };
+}
+
+function tokenizeEmmet(emmetString) {
+  const regex = new RegExp(
+    `(?<tag>[a-z]+)?` +                // tag
+    `(?<attributes>\\[[a-z-0-9=" ]+?\\])?` + // attributes
+    `(?<id>#[a-z-0-9]+)?` +             // id
+    `(?<classes>(?:\\.[a-z-0-9]+)*)?` + // classes
+    `(?<textContent>{[\\s\\S]+?})?`      // text content
+  , 'g');
+
+  const matches = [...emmetString.matchAll(regex)].filter(match => match[0].length > 0);
+  const tokens = [];
+  matches.forEach( (match, index) => {
+    tokens.push(match[0]);
+    let end;
+    if (index + 1 < matches.length) {
+      end = matches[index + 1].index;
+    } 
+    const token = emmetString.slice(match.index + match[0].length, end);
+    if (token.length > 0)
+      tokens.push(token);
+  });
+  const template = document.createElement('template');
+  let last = template.content.appendChild(create(tokens.shift()));
+  let operator = tokens.shift();
+  while (operator) {
+    let operand = tokens.length > 0 ? create(tokens.shift()) : undefined;
+    switch (operator.at(0)) {
+      case '+':
+        last = template.content.appendChild(operand);
+        break;
+      case '>':
+        last = last.appendChild(operand);
+        break;
+      case '*':
+        let num = +operator.slice(1);
+        for(; num > 1; num--) {
+          last.after(last.cloneNode(true));
+        }
+        break;
+    }
+    operator = tokens.shift();
+  }
+  return template;
 }
 
 function attributeMapFromString(attributesString) {
@@ -50,7 +97,9 @@ function elementFromObject(elementObject) {
     });
   }
 
-  node.id = elementObject.id ? elementObject.id : '';
+  if (elementObject.id !== undefined) {
+    node.id = elementObject.id;
+  }
 
   if (elementObject.classes !== undefined) {
     elementObject.classes.forEach(cls => {
@@ -58,13 +107,17 @@ function elementFromObject(elementObject) {
     });
   }
 
-  node.textContent = elementObject.textContent;
+  if (elementObject.textContent !== undefined) {
+    node.textContent = elementObject.textContent;
+  }
+  
   return node;
 }
 
 function create(emmetString) {
-
-  return elementFromObject(parseEmmetToObject(emmetString))
+  return elementFromObject(parseEmmetToObject(emmetString));
 }
 
-export { parseEmmetToObject, create, document, HTMLElement };
+tokenizeEmmet('div#id{hello world}+div[flag-9].class{dfadsf}');
+
+export { tokenizeEmmet, parseEmmetToObject, create, document, Node, HTMLTemplateElement };
